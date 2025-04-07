2be3d58c4ae939fb678078f3dcdd3f506881d00b
G00dzero <134785111+G00dzero@users.noreply.github.com>
2025-04-08 00:36:20 +0100

    add functionalities such as show books that are bak in library and not also return books manually as well as add returned books back into your library giving users time to rent them as well

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,14 @@
             components = new System.ComponentModel.Container();
             dataGridView1 = new DataGridView();
             timer1 = new System.Windows.Forms.Timer(components);
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            RDInStock = new RadioButton();
+            RdOutOfStock = new RadioButton();
+            btnReturn = new Button();
+            btnAdd = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -40,7 +48,7 @@
             dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(763, 286);
+            dataGridView1.Size = new Size(678, 180);
             dataGridView1.TabIndex = 0;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -48,22 +56,107 @@
             // 
             timer1.Tick += timer1_Tick;
             // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            listView1.FullRowSelect = true;
+            listView1.Location = new Point(12, 226);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(678, 121);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Book";
+            columnHeader1.Width = 260;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Author";
+            columnHeader2.Width = 260;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Quantity in AStock";
+            columnHeader3.Width = 260;
+            // 
+            // RDInStock
+            // 
+            RDInStock.AutoSize = true;
+            RDInStock.Location = new Point(25, 353);
+            RDInStock.Name = "RDInStock";
+            RDInStock.Size = new Size(82, 24);
+            RDInStock.TabIndex = 2;
+            RDInStock.TabStop = true;
+            RDInStock.Text = "In Stock";
+            RDInStock.UseVisualStyleBackColor = true;
+            RDInStock.CheckedChanged += RDInStock_CheckedChanged;
+            // 
+            // RdOutOfStock
+            // 
+            RdOutOfStock.AutoSize = true;
+            RdOutOfStock.Location = new Point(25, 383);
+            RdOutOfStock.Name = "RdOutOfStock";
+            RdOutOfStock.Size = new Size(116, 24);
+            RdOutOfStock.TabIndex = 3;
+            RdOutOfStock.TabStop = true;
+            RdOutOfStock.Text = "Out of  Stock";
+            RdOutOfStock.UseVisualStyleBackColor = true;
+            RdOutOfStock.CheckedChanged += RdOutOfStock_CheckedChanged;
+            // 
+            // btnReturn
+            // 
+            btnReturn.Location = new Point(696, 40);
+            btnReturn.Name = "btnReturn";
+            btnReturn.Size = new Size(94, 29);
+            btnReturn.TabIndex = 4;
+            btnReturn.Text = "Return";
+            btnReturn.UseVisualStyleBackColor = true;
+            btnReturn.Click += btnReturn_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(696, 12);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnAdd);
+            Controls.Add(btnReturn);
+            Controls.Add(RdOutOfStock);
+            Controls.Add(RDInStock);
+            Controls.Add(listView1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
         private System.Windows.Forms.Timer timer1;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private RadioButton RDInStock;
+        private RadioButton RdOutOfStock;
+        private Button btnReturn;
+        private Button btnAdd;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ namespace LibraryBookV2
     {
         private List<Books> books; // Make books a class-level variable
         private Dictionary<String, bool> notifiedBooks; // Track notified books
-
+        private List<Books> returnedBooks = new List<Books>();
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +19,15 @@ namespace LibraryBookV2
 
         private void loadData(List<Books> books)
         {
-            DateTime dueDate = DateTime.Now.AddMinutes(2); // Set due date to 4 minutes ahead of the current time
-            Books b = new Books("The Great Gatsby", "F. Scott Fitzgerald", "Fiction", new Stocks(1, 10, null!), dueDate);
-            Books b1 = new Books("To Kill a Mockingbird", "Harper Lee", "Fiction", new Stocks(2, 5, null!), dueDate);
-            Books b2 = new Books("1984", "George Orwell", "Dystopian", new Stocks(3, 8, null!), dueDate);
-            Books b3 = new Books("The Wizard of Oz", "Goody", "fiction", new Stocks(4, 0, null!), dueDate);
+            DateTime dueDate1 = new DateTime(2025, 4, 7, 19, 40, 0); // Set specific due date
+            DateTime dueDate2 = new DateTime(2025, 5, 15, 18, 20, 0); // Set specific due date
+            DateTime dueDate3 = new DateTime(2025, 7, 23, 16, 0, 0); // Set specific due date
+            DateTime dueDate4 = new DateTime(2025, 4, 25, 8, 0, 0); // Set specific due date
+
+            Books b = new Books("The Great Gatsby", "F. Scott Fitzgerald", "Fiction", new Stocks(1, 10, null!), dueDate1);
+            Books b1 = new Books("To Kill a Mockingbird", "Harper Lee", "Fiction", new Stocks(2, 5, null!), dueDate2);
+            Books b2 = new Books("1984", "George Orwell", "Dystopian", new Stocks(3, 8, null!), dueDate3);
+            Books b3 = new Books("The Wizard of Oz", "Goody", "fiction", new Stocks(4, 0, null!), dueDate4);
             books.Add(b);
             books.Add(b1);
             books.Add(b2);
@@ -50,13 +54,12 @@ namespace LibraryBookV2
 
             foreach (Books book in books)
             {
-                if (DateTime.Now >= book.DueDate )
+                if (DateTime.Now >= book.DueDate)
                 {
-                    var result = MessageBox.Show($"The book '{book.Title}' is overdue.\n\nWould you like to return it?", "Book Overdue", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-                    if (result == DialogResult.Yes)
-                    {
-                        booksToRemove.Add(book); // Add book to removal list
-                    }
+                    var result = MessageBox.Show($"The book '{book.Title}' is overdue.\n\nWould you like to return it?", "Book Overdue", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    booksToRemove.Add(book); // Add book to removal list
+
                     notifiedBooks[book.Title] = true;
                     // Mark as notified
                 }
@@ -82,10 +85,10 @@ namespace LibraryBookV2
         private void ReturnBook(Books book)
         {
             books.Remove(book);
+            returnedBooks.Add(book); // Track returned books
             dataGridView1.DataSource = null;
             dataGridView1.DataSource = books;
             notifiedBooks.Remove(book.Title);
-            // Remove from notifiedBooks
         }
 
         private void InitializeTimer()
@@ -93,6 +96,96 @@ namespace LibraryBookV2
             // Initialize and configure the timer
             timer1.Interval = 10500; // Set the interval to 1000 milliseconds (1 second)
             timer1.Tick += timer1_Tick;
+        }
+
+        private void RDInStock_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            if (RDInStock.Checked)
+            {
+                foreach (Books book in returnedBooks)
+                {
+                    ListViewItem item = new ListViewItem(book.Title);
+                    item.SubItems.Add(book.Author);
+                    item.SubItems.Add(book.Genre);
+                    listView1.Items.Add(item);
+                }
+            }
+
+        }
+
+        private void RdOutOfStock_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            if (RdOutOfStock.Checked)
+            {
+                // Show books that are not due yet and sort them by due date in ascending order
+                List<Books> notDueBooks = books.Where(b => b.DueDate > DateTime.Now).OrderBy(b => b.DueDate).ToList();
+
+                foreach (Books book in notDueBooks)
+                {
+                    ListViewItem item = new ListViewItem(book.Title);
+                    item.SubItems.Add(book.Author);
+                    item.SubItems.Add(book.Genre);
+                    item.SubItems.Add(book.DueDate.ToString("yyyy-MM-dd HH:mm")); // Add due date to the list view
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            //selects book from data grid view and click button to return it
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                Books selectedBook = (Books)dataGridView1.SelectedRows[0].DataBoundItem;
+                var result = MessageBox.Show($"Are you sure you want to return '{selectedBook.Title}'?", "Return Book", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    ReturnBook(selectedBook);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a book to return.", "No Book Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //when a book is selected in listview1 and click the button, it will be added to the data grid view
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                string title = selectedItem.Text;
+                string author = selectedItem.SubItems[1].Text;
+                string genre = selectedItem.SubItems[2].Text;
+
+                // Prompt user to input the number of days
+                string input = Interaction.InputBox("Enter the number of days until the book is due:", "Input Days", "3");
+                if (int.TryParse(input, out int days))
+                {
+                    Books newBook = new Books(title, author, genre, new Stocks(0, 0, null!), DateTime.Now.AddDays(days));
+                    books.Add(newBook);
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = books;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input. Please enter a valid number of days.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a book to add.", "No Book Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
